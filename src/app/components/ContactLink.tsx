85966e08371f0a68eace826a911276d27bdd94b0
TheBastosBob <bastiencantet@outlook.fr>
2024-09-20 00:08:58 +0200

    add: style

--- src/app/components/ContactLink.tsx
+++ src/app/components/ContactLink.tsx
@@ -3,19 +3,20 @@ import React from "react";
 
 export function ContactLink() {
     return (
-        <div style={{position: 'absolute',bottom: 0, left: "50%",display:"flex", transform: 'translate(-50%, 0)'
-            ,flexDirection:"column", justifyContent:'end', alignItems:"center"}}>
-            <h1 style={{color: 'black', fontSize: '1.5em'}}>Made with ❤️ by Bastien CANTET</h1>
-            <Link prefetch={false} href="https://github.com/bastiencantet" style={{
-                padding: '10px 20px',
-                backgroundColor: 'black',
-                color: 'white',
-                textDecoration: 'none',
-                borderRadius: '5px',
-                marginTop: '10px',
-                display: 'inline-block',
-                marginBottom: '10px',
-            }}>My Github</Link>
+        <div style={{position: 'absolute', bottom: 0, left: "50%", display: "flex", transform: 'translate(-50%, 0)',
+            flexDirection: "column", justifyContent: 'end', alignItems: "center"
+        }}>
+            <div style={{display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center",
+                padding: '10px', borderRadius: '10px', marginBottom: '20px',
+                paddingLeft: '20px', paddingRight: '20px',
+                boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)', backdropFilter: 'blur(10px)',
+                border: '1px solid rgba(100, 100, 100, 0.2)'
+            }}>
+                <h1 style={{color: 'lightgrey', fontSize: '1.5em', opacity: '1'}}>Made with ❤️ by Bastien CANTET</h1>
+                <Link prefetch={false} href="https://github.com/bastiencantet" style={{
+                    color: 'lightgrey', fontSize: '1em', opacity: '0.8', textDecoration: 'underline',
+                }}>View my Github</Link>
+            </div>
         </div>
     )
 }
