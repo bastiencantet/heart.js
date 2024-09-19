27242779f6fedca26bd0bbe83df5b3cedaf11fb1
TheBastosBob <bastiencantet@outlook.fr>
2024-09-20 00:55:41 +0200

    add: tailwind

--- src/app/components/ContactLink.tsx
+++ src/app/components/ContactLink.tsx
@@ -3,15 +3,10 @@ import React from "react";
 
 export function ContactLink() {
     return (
-        <div style={{position: 'absolute', bottom: 0, left: "50%", display: "flex", transform: 'translate(-50%, 0)',
+        <div className={"w-screen sm:left-1/2 sm:-translate-x-1/2 sm"} style={{position: 'absolute', bottom: 0, display: "flex",
             flexDirection: "column", justifyContent: 'end', alignItems: "center"
         }}>
-            <div style={{display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center",
-                padding: '10px', borderRadius: '10px', marginBottom: '20px',
-                paddingLeft: '20px', paddingRight: '20px',
-                boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)', backdropFilter: 'blur(10px)',
-                border: '1px solid rgba(100, 100, 100, 0.2)'
-            }}>
+            <div className={"card w-screen sm:w-auto"}>
                 <h1 style={{color: 'lightgrey', fontSize: '1.5em', opacity: '1'}}>Made with ❤️ by Bastien CANTET</h1>
                 <Link prefetch={false} href="https://github.com/bastiencantet" style={{
                     color: 'lightgrey', fontSize: '1em', opacity: '0.8', textDecoration: 'underline',
