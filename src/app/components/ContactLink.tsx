2363cde91dc21ed433ac6ed66059c2177c539f21
TheBastosBob <bastiencantet@outlook.fr>
2024-09-19 23:46:08 +0200

    add: link

--- src/app/components/ContactLink.tsx
+++ src/app/components/ContactLink.tsx
@@ -6,7 +6,7 @@ export function ContactLink() {
         <div style={{position: 'absolute',bottom: 0, left: "50%",display:"flex", transform: 'translate(-50%, 0)'
             ,flexDirection:"column", justifyContent:'end', alignItems:"center"}}>
             <h1 style={{color: 'black', fontSize: '1.5em'}}>Made with ❤️ by Bastien CANTET</h1>
-            <Link prefetch={false} href="" style={{
+            <Link prefetch={false} href="https://github.com/bastiencantet" style={{
                 padding: '10px 20px',
                 backgroundColor: 'black',
                 color: 'white',
